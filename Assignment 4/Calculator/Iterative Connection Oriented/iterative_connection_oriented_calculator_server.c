e4c44c79d62c89b742bf84ebcaea3c245957e36f
Maureen Njihia <maureennjihia468@gmail.com>
2023-05-11 15:22:43 +0300

    Modify the server function

--- Assignment 4/Calculator/Iterative Connection Oriented/iterative_connection_oriented_calculator_server.c
+++ Assignment 4/Calculator/Iterative Connection Oriented/iterative_connection_oriented_calculator_server.c
@@ -15,23 +15,27 @@ calculate_1_svc(calculator_input *argp,  struct svc_req *rqstp)
     printf("Calculate Procedure Called\n");
 
     static int result;
-    printf("Received operator: %c\n", *argp->op);
+    printf("Data from client: %d %c %d\n", argp->num1, *argp->op, argp->num2);
     switch (*argp->op) {
         case '+':
             result = argp->num1 + argp->num2;
+            printf("Sum is %d\n", result);
             break;
         case '-':
             result = argp->num1 - argp->num2;
+            printf("Difference is %d\n", result);
             break;
         case '*':
             result = argp->num1 * argp->num2;
+            printf("Product is %d\n", result);
             break;
         case '/':
-            if(argp->num2 == 0) {
+            if (argp->num2 == 0) {
                 printf("Division by zero\n");
                 exit(1);
             }
             result = argp->num1 / argp->num2;
+            printf("Quotient is %d\n", result);
             break;
         default:
             printf("Invalid operator\n");
