d862ed7d98783b3f190717b291ff7daed51f0411
Maureen Njihia <maureennjihia468@gmail.com>
2023-06-13 10:14:35 +0300

    Remove while loop from client

--- Assignment 2/Iterative Connectionless/calculator_iterative_connectionless_client.c
+++ Assignment 2/Iterative Connectionless/calculator_iterative_connectionless_client.c
@@ -28,56 +28,55 @@ int main(int argc, char const *argv[]) {
         return -1;
     }
 
-    while (1) {
-        // Prompt the user for input
-        int num1, num2;
-        char op;
-        printf("Enter the first number: ");
-        scanf("%d", &num1);
-        printf("Choose the operator you would like to use:\n");
-        printf("+ (Addition)\n");
-        printf("- (Subtraction)\n");
-        printf("* (Multiplication)\n");
-        printf("/ (Division)\n");
-        scanf(" %c", &op);
-        printf("Enter the second number: ");
-        scanf("%d", &num2);
+    // Prompt the user for input
+    int num1, num2;
+    char op;
+    printf("Enter the first number: ");
+    scanf("%d", &num1);
+    printf("Choose the operator you would like to use:\n");
+    printf("+ (Addition)\n");
+    printf("- (Subtraction)\n");
+    printf("* (Multiplication)\n");
+    printf("/ (Division)\n");
+    scanf(" %c", &op);
+    printf("Enter the second number: ");
+    scanf("%d", &num2);
 
-        // Check if the operator entered is valid
-        switch (op) {
-            case '+':
-            case '-':
-            case '*':
-            case '/':
-                break;
-            default:
-                printf("Invalid operator\n");
-                continue;
-        }
-
-        // Construct the request string
-        snprintf(buffer, 1024, "%d %c %d", num1, op, num2);
-
-        // Send the request to the server
-        if (sendto(sock, buffer, strlen(buffer), 0, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
-            printf("\nSendto failed\n");
+    // Check if the operator entered is valid
+    switch (op) {
+        case '+':
+        case '-':
+        case '*':
+        case '/':
+            break;
+        default:
+            printf("Invalid operator\n");
             continue;
-        }
-
-        // Receive the response from the server
-        socklen_t serv_addr_len = sizeof(serv_addr);
-        if (recvfrom(sock, buffer, 1024, 0, (struct sockaddr *)&serv_addr, &serv_addr_len) < 0) {
-            printf("\nRecvfrom failed\n");
-            continue;
-        }
-
-        // Extract the result from the response
-        int result = atoi(buffer);
-
-        // Display the result to the user
-        printf("Result: %d\n", result);
-        printf("\n");
     }
+
+    // Construct the request string
+    snprintf(buffer, 1024, "%d %c %d", num1, op, num2);
+
+    // Send the request to the server
+    if (sendto(sock, buffer, strlen(buffer), 0, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+        printf("\nSendto failed\n");
+        continue;
+    }
+
+    // Receive the response from the server
+    socklen_t serv_addr_len = sizeof(serv_addr);
+    if (recvfrom(sock, buffer, 1024, 0, (struct sockaddr *)&serv_addr, &serv_addr_len) < 0) {
+        printf("\nRecvfrom failed\n");
+        continue;
+    }
+
+    // Extract the result from the response
+    int result = atoi(buffer);
+
+    // Display the result to the user
+    printf("Result: %d\n", result);
+    printf("\n");
+    
 
     return 0;
 }
